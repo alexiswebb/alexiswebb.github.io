43d93f94f1a1dfea8dc1eb743e67db8f4d9280d5
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-10-19 12:47:43 -0700

    Updates

---
+++ precache-manifest.6c8ed63ba732efa5269b45c235e042ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7a94f720c768daa413a98a0ae775e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0916a5aa9558d92c579b",
+    "url": "/static/css/main.65f8b90d.chunk.css"
+  },
+  {
+    "revision": "541392f22bd0e2b47559",
+    "url": "/static/js/2.d84d1adc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d84d1adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0916a5aa9558d92c579b",
+    "url": "/static/js/main.8f6c2712.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
