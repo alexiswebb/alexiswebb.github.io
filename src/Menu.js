26e4e6cd07613a1898658514a787122858cecb51
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-10-19 12:41:11 -0700

    Add React and routing in menu

---
+++ src/Menu.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Projects from "./Projects";
+import Resume from "./Resume";
+ 
+class Menu extends Component {
+  render() {
+    return (
+      <HashRouter>
+	<div>
+	  <h1>Alexis Webb</h1>
+	  <ul>
+	    <li><NavLink exact to="/">Projects</NavLink></li>
+	    <li><NavLink to="/resume">Resume</NavLink></li>
+	    <li><a href='mailto:alexniwebb@gmail.com'>Email</a></li>
+	    <li><a href='https://github.com/alexiswebb'>Github</a></li>
+	    <li><a href='https://www.linkedin.com/in/alexis-webb-41039277/'>LinkedIn</a></li>
+	  </ul>
+          <div className="content">
+            <Route exact path="/" component={Projects}/> 
+            <Route path="/resume" component={Resume}/> 
+          </div>
+	</div>
+      </HashRouter>
+    );
+  }
+}
+ 
+export default Menu;
---
+++ src/Projects.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./Projects.css";
+ 
+class Projects extends Component {
+  render() {
+  return (
+      <div class="project-container">
+	<div class="badge-container-left">
+	  <div class="badge">
+	    <img src="headphones.jpg" />
+	  </div>
+	</div>
+	<div class="project-description text-right">
+	  <h3><a href= "/">Spotify Playlist Generator</a></h3>
+	  <div>A set of APIs used to generate Spotify playlists</div>
+	</div>
+	<div class="project-description text-left">
+	  <h3><a href= "/">Correlation Finder</a></h3>
+	  <div>Suggests predictive capacity of fields in a csv</div>
+	</div>
+	<div class="badge-container-right">
+	  <div class="badge">
+	    <img src="dots.png" />
+	  </div>
+	</div>
+	<div class="badge-container-left">
+	  <div class="ascii-badge">
+	    <img src="wave_small.png" />
+	  </div>
+	</div>
+	<div class="project-description text-right">
+	  <h3><a href= "/">ASCII Image Converter</a></h3>
+	  <div>Upload a picture and convert it into ASCII art</div>
+	</div>
+      </div>
+    );
+  }
+}
+ 
+export default Projects;
